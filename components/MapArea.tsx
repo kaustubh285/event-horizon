546503c57df6dbe21aa9dc314720f10c3e00bd22
kaustubh285 <kaustubh285@gmail.com>
2024-07-04 11:47:47 +0100

    Clustering of markers complete

--- components/MapArea.tsx
+++ components/MapArea.tsx
@@ -4,6 +4,7 @@ import FlyToMarker from "./FlyToMarker";
 import { Event, Location } from "@/typings";
 import { convertTZ, getCategoryIcon } from "@/utils/DefaultHelper";
 import Link from "next/link";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 type Props = {
   defaultPosition: Location;
@@ -29,31 +30,31 @@ const MapArea = ({
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution={`&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
       />
+      <MarkerClusterGroup chunkedLoading>
+        {allEvents?.map((eve: Event) => {
+          // const position = [...eve.geometries[0]?.coordinates].reverse();
 
-      {allEvents?.map((eve: Event) => {
-        // const position = [...eve.geometries[0]?.coordinates].reverse();
-
-        return (
-          <Marker
-            key={eve.id}
-            riseOnHover
-            position={
-              [
-                eve?.geometries[0].coordinates[0],
-                eve?.geometries[0].coordinates[1] - 0.002,
-              ] || defaultPosition
-            }
-            icon={getCategoryIcon(eve.categories[0].title)}
-            eventHandlers={{
-              click: () => selectEvent(eve),
-            }}>
-            <Tooltip direction='bottom' offset={[-20, 10]} opacity={0.7}>
-              {eve.title}
-            </Tooltip>
-          </Marker>
-        );
-      })}
-
+          return (
+            <Marker
+              key={eve.id}
+              riseOnHover
+              position={
+                [
+                  eve?.geometries[0].coordinates[0],
+                  eve?.geometries[0].coordinates[1] - 0.002,
+                ] || defaultPosition
+              }
+              icon={getCategoryIcon(eve.categories[0].title)}
+              eventHandlers={{
+                click: () => selectEvent(eve),
+              }}>
+              <Tooltip direction='bottom' offset={[-20, 10]} opacity={0.7}>
+                {eve.title}
+              </Tooltip>
+            </Marker>
+          );
+        })}
+      </MarkerClusterGroup>
       {selectedEvent && (
         <>
           <Popup
